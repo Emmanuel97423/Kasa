f2a39b3ea6f8717ee93bc10aeecdef89cf40fee6
Emmanuel Narcisse <epokweb@hotmail.fr>
2022-11-11 19:10:42 +0400

    Feature: footer & margin for list content ok

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../assets/img/bottom_logo.png';
+import styles from './Footer.module.css';
+
+export default function Footer() {
+      return (
+            <div className={styles.footer}>
+                  <img src={logo} alt="bottom_logo" />
+                  <p>2022 Kasa.All rights reserved</p>
+            </div>
+      );
+}
--- src/layout/Default/index.jsx
+++ src/layout/Default/index.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from '../../components/NavBar';
+import Footer from '../../components/Footer';
 import styles from './Default.module.css';
 
 export default function DefaultLayout() {
       return (
-            <div className={styles.app}>
-                  <header className={styles.header}>
-                        <NavBar />
-                  </header>
-                  <section id="content" className="app-content">
-                        <Outlet />
+            <div>
+                  <section className={styles.app}>
+                        <header className={styles.header}>
+                              <NavBar />
+                        </header>
+                        <section id="content" className="app-content">
+                              <Outlet />
+                        </section>
                   </section>
-                  <footer>Footer</footer>
+                  <Footer />
             </div>
       );
 }
