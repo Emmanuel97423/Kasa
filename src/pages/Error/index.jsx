c0e018658f1203dc691141dbf08287bea611a2b8
Emmanuel Narcisse <epokweb@hotmail.fr>
2022-11-14 16:23:20 +0400

    Error page & layout OK

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import NavBar from '../../components/NavBar';
+import layout from '../../layout/Default/Default.module.css';
+import styles from './Error.module.css';
 
 export default function ErrorPage() {
       // const error = useRouteError(ErrorPage);
       return (
-            <div>
-                  <h1>404</h1>
-                  <p>Oups! La page que vous demandez n&#39; sexiste pas.</p>
-                  <p>
+            <section className={layout.app}>
+                  <header className={layout.header}>
+                        <NavBar />
+                  </header>
+                  <section className={styles.errorContent}>
+                        <h1>404</h1>
+                        <p>Oups! La page que vous demandez n&#39;existe pas.</p>
                         <Link to="/">Retourner sur la page d&#39;accueil</Link>
-                  </p>
-            </div>
+                  </section>
+            </section>
       );
 }
