703a18f4bcdbb95c0012e507efba2504116afb4f
Emmanuel Narcisse <epokweb@hotmail.fr>
2022-11-29 11:56:04 +0400

    Feature: rating star OK

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
-import Property from './pages/Property';
+import House from './pages/House';
 import About from './pages/About';
 // import DefaultLayout from './layout/Default';
 import ErrorPage from './pages/Error';
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
                   },
                   {
                         path: '/property/:id',
-                        element: <Property />,
+                        element: <House />,
                   },
                   {
                         path: '/a-propos',
@@ -39,7 +39,9 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-      <RouterProvider router={router} />
+      <React.StrictMode>
+            <RouterProvider router={router} />
+      </React.StrictMode>
 );
 
 // root.render(
